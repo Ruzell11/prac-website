8e7c25af3c4f8fa657863e39434e57f93fe52794
Ruzell11 <johnruzell123@gmail.com>
2022-06-25 18:32:44 +0800

    add cards

--- index.js
+++ index.js
@@ -10,4 +10,36 @@ body.forEach((item) => {
   item.addEventListener('click' , () => {
     navLinks.classList.remove('active')
   })
-})
+  })
+//1.
+$(() => {
+  $('.OrangeHead').each(function() {
+    var text_words = $(this).text().split(' ');
+      $(this).empty().html(function() {
+        for (i = 0; i < text_words.length; i++) {
+          if (i === 0) {
+            $(this).append('<span>' + text_words[i] + '</span>');
+          } else {
+            $(this).append(' <span>' + text_words[i] + '</span>');
+          }
+        }
+      });
+ 
+
+      // $(window).on('scroll' ,() => {
+      //   if($(this).scrollTop() > 100){
+      //     $('#scroll').css('opacity' , "1")
+      //   }else{
+      //     $('#scroll').css('opacity' , "0")
+      //   }
+      // })
+      // $('#scroll').on('click', function() {
+      //   $('html, body').animate({
+      //     scrollTop: 0
+      //   }, 500);
+      //   return false;
+      // })
+
+  })
+  
+});
